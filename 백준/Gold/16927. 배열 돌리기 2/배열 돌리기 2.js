4fb329cacfd58fa3525b4724a5f48b6c2b7f69cd
SeEun Park <pse6704@sookmyung.ac.kr>
2024-04-16 01:33:11 +0900

    Update 배열 돌리기 2.js

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.js
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.js
@@ -1,9 +1,12 @@
+// 동일한 문제 16926번 에서는 dx dy로 문제 풀었으나, 시간복잡도 초과 이슈로 단순 반복문으로 변경 
 // readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
 let fs = require("fs"); 
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [n, m, count] = input[0].split(' ');
+const [n, m, r] = input[0].split(' ');
 let arr = [];
+// 배열 초기화 - 더 나은 방법
+// let arr = input.slice(1).map(x=>x.split(' ').map(Number))
 for ( let i = 1; i <= n; i++){ 
   arr.push(input[i].split(' '));  
 }
@@ -22,11 +25,12 @@ function rotateMatrix(N, M, R, matrix) {
         for (let i = lastCol - 1; i >= firstCol; i--) temp.push(matrix[lastRow][i]); // 하
         for (let i = lastRow - 1; i > firstRow; i--) temp.push(matrix[i][firstCol]); // 좌
         
-        let size = temp.length;
-        let shift = R % size;  // 실제 회전 수 계산
+        let size = temp.length; // n*m - (n-2)*(m-2) 와 동일. 테두리 요소의 수
+        let count = R % size;  // 실제 회전 수 계산
 
         // 회전 처리
-        temp = [...temp.slice(shift), ...temp.slice(0, shift)];
+        // 구조 분해 할당으로 리스트 합치기 + slice(count)로 count index 부터 ~
+        temp = [...temp.slice(count), ...temp.slice(0, count)];
 
         // 회전된 내용을 행렬에 다시 채우기
         let index = 0;
@@ -40,7 +44,7 @@ function rotateMatrix(N, M, R, matrix) {
 }
 
 // 함수 실행 및 결과 출력
-const result = rotateMatrix(n, m, count, arr);
+const result = rotateMatrix(n, m, r, arr);
 result.forEach(row => console.log(row.join(' ')));
 
 
