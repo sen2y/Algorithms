0c8d694b1033f7ecafaa1b7716412ff6328a080f
SeEun Park <pse6704@sookmyung.ac.kr>
2024-04-16 18:10:45 +0900

    Update 배열 돌리기 1.js

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.js
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.js
@@ -2,51 +2,51 @@
 let fs = require("fs"); 
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [n, m, count] = input[0].split(' ');
+const [n, m, count] = input[0].split(' '); // n: 세로, m: 가로, count: 회전 횟수
 let arr = [];
-for ( let i = 1; i <= n; i++){ 
+for ( let i = 1; i <= n; i++){  // 배열 입력
   arr.push(input[i].split(' '));  
 }
 
-function inRange(x,y,flag,n,m) { 
+function inRange(x,y,flag,n,m) {  // 범위 체크
   return x >= flag && x < flag+n && y >=flag && y < flag+m
 }
 
 let dx = [-1, 0, 1, 0] // 북 동 남 서
 let dy = [0, 1, 0, -1]
-let direction = 3;
+let direction = 3; // 서쪽
 let x = 0;
 let y = 1;
 
-let prev = 0; 
-let tmp = 0;
+let prev = 0; // 이전 값 저장
+let tmp = 0; // 임시 저장
 for (let i = 0; i<count; i++){
-  x = 0;
-  y = 1;
-  prev = arr[x][y]; 
-  tmp = 0; 
-  rotateArr(arr, n, m, 0)
+  x = 0; 
+  y = 1; 
+  prev = arr[x][y]; // 시작점 
+  tmp = 0; // 초기화
+  rotateArr(arr, n, m, 0) // 겉 테두리 반시계 회전
 }
 
 // 정답 출력
 arr.forEach((array)=>{console.log(...array)})
 
 function rotateArr(arr, n, m, flag) { // 겉 테두리 반시계 회전
-  if (n<=0 || m<=0) {
+  if (n<=0 || m<=0) { // n이나 m이 0이면 종료
     return arr;
   }
-  for (let i = 0; i < n*m - (n-2)*(m-2); i++ ){
-    let px = x+dx[direction];
+  for (let i = 0; i < n*m - (n-2)*(m-2); i++ ){ // n*m - (n-2)*(m-2) : 테두리의 길이
+    let px = x+dx[direction];  
     let py = y+dy[direction];  
     if (!inRange(px, py, flag,n,m)) { 
-      direction = (direction -1 + 4) % 4;
+      direction = (direction -1 + 4) % 4; // 반시계 방향 전환
       
     }
-    x = x+dx[direction];
+    x = x+dx[direction]; 
     y = y+dy[direction]; 
-    tmp = arr[x][y];
-    arr[x][y] = prev;
-    prev = tmp;  
+    tmp = arr[x][y]; // 현재 값 저장
+    arr[x][y] = prev; // 현재 값에 이전 값 저장
+    prev = tmp;  // 이전 값에 현재 값 저장
   }  
   // 재귀함수 실행 전, x,y,prev 초기화
   x += 1;
@@ -55,7 +55,6 @@ function rotateArr(arr, n, m, flag) { // 겉 테두리 반시계 회전
   flag ++; // inRange 설정위한 기준
   rotateArr(arr,n-2,m-2, flag) // 재귀함수, 그다음 겉 테두리 반시계 회전
 }
-
 
 
 
