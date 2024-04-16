e2efa5c320245cfb510a489045712a6193d48c89
SeEun Park <pse6704@sookmyung.ac.kr>
2024-04-16 18:03:12 +0900

    Update 달팽이2.js

--- 백준/Bronze/1952. 달팽이2/달팽이2.js
+++ 백준/Bronze/1952. 달팽이2/달팽이2.js
@@ -2,7 +2,7 @@ let fs = require("fs");
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [rows, columns] = input[0].split(' ').map(Number); // 각자 숫자로 형변환
-const  arr = Array.from(new Array(rows), () => new Array(columns).fill(0));
+const  arr = Array.from(new Array(rows), () => new Array(columns).fill(0)); // 0으로 초기화된 2차원 배열 생성  
 
 function inRange(x,y) { // 범위 체크 함수
   return x>=0 && x <rows && y>=0 && y<columns;
@@ -15,17 +15,19 @@ let x=0;
 let y=0;
 let direction = 1; // 기본 방향
 let count=0; // 제출 값
-arr[0][0] = 1;
+arr[0][0] = 1; // 시작점
 
 for (let i=2; i<=columns*rows; i++){
-  let nx = x+dx[direction];
+  // 현재 방향으로 다음 좌표
+  let nx = x+dx[direction]; 
   let ny = y+dy[direction];
   
-  if ( !inRange(nx,ny)|| arr[nx][ny] !== 0) {  // 범위 밖이거나, 이미 들른 곳인 경우
+  if ( !inRange(nx,ny)|| arr[nx][ny] !== 0) {  // 다음 좌표가 범위 밖이거나, 이미 들른 곳인 경우
     direction = (direction+1) % 4; // 방향 전환
     count++; // 제출 값 ++
   }
-  x = x+dx[direction];
+  // 방향 업데이트 후 다시 현재 방향으로 다음 좌표
+  x = x+dx[direction]; 
   y = y+dy[direction]; 
   arr[x][y] = i; 
 } 
