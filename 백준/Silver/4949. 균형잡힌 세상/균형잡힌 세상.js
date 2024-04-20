0c177af0a36e241c5335b2f9d9f2b7a4292b07bc
SeEun Park <pse6704@sookmyung.ac.kr>
2024-04-20 21:26:27 +0900

    Update 균형잡힌 세상.js

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
@@ -8,25 +8,27 @@ for (str in arr) {
   let stack = [];
   let flag = true;
   if (!arr[str].slice(0, -1).length) continue; // 입력값이 . 하나인 경우 넘어가기
-  // word.forEach((x) => {
+  // word.forEach((x) => { // forEach문에서 break문 사용 불가. for문으로 변경
   for (let i = 0; i < word.length; i++) {
     const x = word[i]; 
-    if (x == "(" || x == "[" || x == ")" || x == "]") {
+    if (x == "(" || x == "[" || x == ")" || x == "]") { // 입력값이 대/소괄호인 경우에만 조건문으로
      
       if (
-        (!stack.length && x == ")") ||
+        (!stack.length && x == ")") || // 빈배열에 닫힌 괄호 들어오면 for문 탈출
         (!stack.length && x == "]")
-         ) {
+      ) {
         flag = false;
-        break;
+        break; // for문 탈출
+        
         
       } else if (
         (stack[stack.length - 1] == "[" && x == "]") ||
         (stack[stack.length - 1] == "(" && x == ")")
       ) {  
         stack.pop();
-      } else if (
-        (stack[stack.length - 1] == "[" && x == ")") ||
+      } else if ( 
+        // 입력이 닫는 괄호인데 직전 괄호가 다른 종류의 열린 괄호일때 for문 탈출
+        (stack[stack.length - 1] == "[" && x == ")") || 
         (stack[stack.length - 1] == "(" && x == "]")
       ) {
         flag = false
