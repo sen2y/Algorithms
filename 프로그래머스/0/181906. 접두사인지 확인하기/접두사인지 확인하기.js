35239794d01978fb49481e119b879f21c9c03e62
SeEun Park <pse6704@sookmyung.ac.kr>
2024-05-02 22:57:29 +0900

    Update 접두사인지 확인하기.js

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,3 +1,7 @@
+// Boolean 값(True, False)를 숫자로(1,0) 바꾸기 + 
+// + 단항 연산자를 사용하여 결과를 숫자로 변환합니다. 따라서 number가 n 또는 m으로 나누어 떨어지지 않으면 0이 되고, 나머지 경우에는 1이 됩니다.
+
+
 function solution(my_string, is_prefix) {
     
     return +my_string.startsWith(is_prefix) 
