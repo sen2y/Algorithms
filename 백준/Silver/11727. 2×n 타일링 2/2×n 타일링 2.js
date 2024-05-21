b8944aab361573b8e3b3d128f7d61f838a3407b4
SeEun Park <pse6704@sookmyung.ac.kr>
2024-05-21 14:20:40 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 200 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const N = Number(input[0]);
+let d = new Array(N+1).fill(0);
+d[0] = 1;
+d[1] = 1;
+for (let i = 2; i <= N; i++){ 
+    d[i] = (2*d[i-2] + d[i-1]) % 10007 ;
+} 
+console.log(d[N])
