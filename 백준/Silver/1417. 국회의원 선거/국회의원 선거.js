0b71a917633dc82148cb8f55b24a9dda1d5b5f13
SeEun Park <pse6704@sookmyung.ac.kr>
2024-05-28 14:41:46 +0900

    [Silver V] Title: 국회의원 선거, Time: 152 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.js
@@ -0,0 +1,123 @@
+module.exports = PriorityQueue;
+
+function PriorityQueue(comparator) {
+  this._comparator = comparator || PriorityQueue.DEFAULT_COMPARATOR;
+  this._elements = [];
+}
+
+PriorityQueue.DEFAULT_COMPARATOR = function(a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a - b;
+  } else {
+    a = a.toString();
+    b = b.toString();
+
+    if (a == b) return 0;
+
+    return (a > b) ? 1 : -1;
+  }
+};
+
+PriorityQueue.prototype.isEmpty = function() {
+  return this.size() === 0;
+};
+
+
+PriorityQueue.prototype.peek = function() {
+  if (this.isEmpty()) throw new Error('PriorityQueue is empty');
+
+  return this._elements[0];
+};
+
+
+PriorityQueue.prototype.deq = function() {
+  var first = this.peek();
+  var last = this._elements.pop();
+  var size = this.size();
+
+  if (size === 0) return first;
+
+  this._elements[0] = last;
+  var current = 0;
+
+  while (current < size) {
+    var largest = current;
+    var left = (2 * current) + 1;
+    var right = (2 * current) + 2;
+
+    if (left < size && this._compare(left, largest) >= 0) {
+      largest = left;
+    }
+
+    if (right < size && this._compare(right, largest) >= 0) {
+      largest = right;
+    }
+
+    if (largest === current) break;
+
+    this._swap(largest, current);
+    current = largest;
+  }
+
+  return first;
+};
+
+PriorityQueue.prototype.enq = function(element) {
+  var size = this._elements.push(element);
+  var current = size - 1;
+
+  while (current > 0) {
+    var parent = Math.floor((current - 1) / 2);
+
+    if (this._compare(current, parent) <= 0) break;
+
+    this._swap(parent, current);
+    current = parent;
+  }
+
+  return size;
+};
+
+PriorityQueue.prototype.size = function() {
+  return this._elements.length;
+};
+
+PriorityQueue.prototype.forEach = function(fn) {
+  return this._elements.forEach(fn);
+};
+
+PriorityQueue.prototype._compare = function(a, b) {
+  return this._comparator(this._elements[a], this._elements[b]);
+};
+
+PriorityQueue.prototype._swap = function(a, b) {
+  var aux = this._elements[a];
+  this._elements[a] = this._elements[b];
+  this._elements[b] = aux;
+};
+
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const n = Number(input[0]);
+let me = Number(input[1]);
+const x =input.slice(2).map(Number); // 본인 투표수 제외한 나머지 줄들을 숫자로 변환하여 배열 x에 저장
+
+let pq = new PriorityQueue((a, b) => a - b); // 내림차순
+
+for (let num of x) {  
+    pq.enq(num);
+} 
+let sum = 0;
+function solution() {
+    if (n==1) return console.log(sum);
+    let deq = pq.deq();
+    while (deq >= me) {
+        sum+=1;
+        me+=1;
+        pq.enq(deq-1);
+        deq = pq.deq();
+    }
+    console.log(sum)
+}
+
+solution();
