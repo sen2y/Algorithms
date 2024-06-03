bc78d998e7efdfa8c9c9465202382185d8209c2f
SeEun Park <pse6704@sookmyung.ac.kr>
2024-06-04 02:41:06 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+-- 생일이 3월인 여성 
+-- 회원의 ID, 이름, 성별, 생년월일
+-- 전화번호 NULL인경우 제외 
+-- 회원 ID 오름 차순 
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE MONTH(DATE_OF_BIRTH) = 3 
+AND GENDER = 'W'
+AND NOT TLNO IS NULL
+ORDER BY MEMBER_ID;
+
