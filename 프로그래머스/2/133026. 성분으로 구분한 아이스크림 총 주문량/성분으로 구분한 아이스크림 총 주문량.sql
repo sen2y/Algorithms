c97b489d67b608e79a7bb74bdb6338384441f97c
SeEun Park <pse6704@sookmyung.ac.kr>
2024-06-04 23:50:33 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+-- 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량
+-- 총주문량을 나타내는 컬럼명은 TOTAL_ORDER로 지정
+-- 총주문량이 작은 순서대로 조회
+SELECT I.INGREDIENT_TYPE, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+FROM FIRST_HALF AS F
+JOIN ICECREAM_INFO AS I ON F.FLAVOR = I.FLAVOR
+GROUP BY INGREDIENT_TYPE
+ORDER BY TOTAL_ORDER;
