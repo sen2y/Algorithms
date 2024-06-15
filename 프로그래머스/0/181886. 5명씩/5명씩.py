3b4492547d79733d6cc2ab915d9a25a45f0324ab
SeEun Park <pse6704@sookmyung.ac.kr>
2024-06-15 18:19:34 +0900

    [level 0] Title: 5명씩, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -1,9 +1,12 @@
 def solution(names):
-    num = len(names) // 5
-    newArr = []
-    for i in range(num):
-        newArr.append(names[i*5]) 
-    if len(names) > num*5 :
-        newArr.append(names[num*5])
-    print(newArr)
-    return newArr
+    # 간단 풀이
+    return names[::5]
+    # 나의 풀이
+    # num = len(names) // 5
+    # newArr = []
+    # for i in range(num):
+    #     newArr.append(names[i*5]) 
+    # if len(names) > num*5 :
+    #     newArr.append(names[num*5])
+    # print(newArr)
+    # return newArr
