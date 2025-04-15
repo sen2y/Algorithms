69f0037efc9df0fafe5e61ab187d93f126a42923
SeEun Park <pse6704@sookmyung.ac.kr>
2025-04-15 15:22:20 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,7 +1,5 @@
-# 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return
-def solution(angle):
-    answer = 0
-    if (angle > 0 and angle < 90): return 1
-    elif(angle == 90): return 2
-    elif(angle > 90 and angle < 180): return 3
-    elif(angle == 180): return 4 
+def solution(angle): 
+    if angle < 90: return 1
+    elif angle == 90: return 2
+    elif angle == 180: return 4
+    return 3
