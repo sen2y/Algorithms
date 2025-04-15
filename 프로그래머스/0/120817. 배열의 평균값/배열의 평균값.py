363a690258b25f6ec1bb6b798641e11d8ea2705a
SeEun Park <pse6704@sookmyung.ac.kr>
2025-04-15 15:49:29 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,10 +1,3 @@
-# def solution(numbers):
-#     answer = 0
-#     sum = 0
-#     for number in numbers:
-#         sum += number 
-#     answer = sum / len(numbers) 
-#     return answer
-
-def solution(numbers):
-    return sum(numbers) / len(numbers)
+def solution(numbers): 
+    total = sum(numbers)
+    return total/len(numbers)
