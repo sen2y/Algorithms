18665ef545151db4b6a68c08a6a154c0e5aebf1e
SeEun Park <pse6704@sookmyung.ac.kr>
2025-04-16 11:40:53 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,10 +1,4 @@
 def solution(my_string, num1, num2): 
-    temp = my_string[num1]
-    answer=''
-    for idx, i in enumerate(my_string):
-        if idx == num1:
-            answer+=my_string[num2]
-        elif idx == num2:
-            answer+=temp
-        else: answer+=i
-    return answer
+    s = list(my_string)
+    s[num1], s[num2] = s[num2], s[num1]
+    return ''.join(s)
