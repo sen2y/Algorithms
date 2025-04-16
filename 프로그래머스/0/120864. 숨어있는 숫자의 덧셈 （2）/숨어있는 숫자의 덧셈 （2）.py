576ca57eca38d108d413c61d09420064d9dc1a07
SeEun Park <pse6704@sookmyung.ac.kr>
2025-04-16 14:05:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,19 +1,15 @@
 def solution(my_string):  
-    count = 0
-    temp = ''
-    for idx, i in enumerate(my_string):
-        print(0, i)
-        if i.isdigit():
-            temp+=i
-            if idx == len(my_string)-1: 
-                print(222, i, temp)
-                count+=int(temp)
-        else:
-            if temp!='': 
-                print(1, temp)
-                count+=int(temp)
-            temp=''
-    print(2, count)
-    return count
-
-solution('12aaa33aa2')
+    # count = 0
+    # temp = ''
+    # for idx, i in enumerate(my_string): 
+    #     if i.isdigit():
+    #         temp+=i
+    #         if idx == len(my_string)-1:  
+    #             count+=int(temp)
+    #     else:
+    #         if temp!='':  
+    #             count+=int(temp)
+    #         temp='' 
+    # return count
+    return sum(map(int,''.join(i if i.isdigit() else ' ' for i in my_string).split()))
+ 
