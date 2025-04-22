bfb524dfb50c6127ea28e37f765dfa6e7c250625
SeEun Park <pse6704@sookmyung.ac.kr>
2025-04-22 13:53:43 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.08 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,15 +1,3 @@
 def solution(sides):
-    fir, sec = sorted(sides)
-    print(fir, sec)
-    cnt = 0 
-    # sec이 가장 긴 변일떄
-    # fir + new > sec 이어야함
-    # new > sec - fir
-    # sec - fir < new < sec
-    for i in range(sec-fir, sec): cnt+=1
-    # new가 가장 긴 변일때
-    # fir + sec > new > sec
-    for j in range(sec, fir+sec): cnt+=1
-    return cnt-1
-        
-            
+    answer = 0
+    return answer
