9e967f3d904ce3ef9d1f7ce142db900bbd92fe41
SeEun Park <pse6704@sookmyung.ac.kr>
2025-04-22 14:12:34 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,19 +1,25 @@
-range = {
-        "left": [-1, 0],
-        "right": [1, 0],
-        "up": [0,1],
-        "down": [0,-1]
+directions = {
+    "left": (-1, 0),
+    "right": (1, 0),
+    "up": (0, 1),
+    "down": (0, -1)
     }
 
-def solution(keyinput, board):
-    print(keyinput, board)
-    result = [0,0]  
+def solution(keyinput, board): 
     # board의 정중앙 0,0에서 시작
+    x, y = 0,0
+    x_limit = board[0] // 2
+    y_limit = board[1] // 2
+
     # board의 크기를 벗어난 방향키 입력은 무시
     for i in keyinput: 
-        if abs(result[0]+range[i][0]) <= board[0]//2: result[0] += range[i][0]
-        if abs(result[1]+range[i][1]) <= board[1]//2: result[1] += range[i][1]
-    return result
+        dx, dy = directions[i]
+        nx, ny = x + dx, y + dy
+        if -x_limit <= nx <= x_limit:
+            x = nx
+        if -y_limit <= ny <= y_limit:
+            y = ny
+    return [x,y]
      
 # 반례 ["right", "right", "right", "right", "right", "left"], [9, 5]
 solution( ["right", "right", "right", "right", "right", "left"], [9, 5])
