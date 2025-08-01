91c410e6cec95cacbb0a0933f007a5a00c3affd1
SeEun Park <pse6704@sookmyung.ac.kr>
2025-08-02 03:25:11 +0900

    [Silver IV] Title: 암기왕, Time: 1524 ms, Memory: 229280 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    note1 = set(map(int, input().split()))
+    M = int(input())
+    note2 = list(map(int, input().split()))
+
+    for num in note2:
+        print(1 if num in note1 else 0)
