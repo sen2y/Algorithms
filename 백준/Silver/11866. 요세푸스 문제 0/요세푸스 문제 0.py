643f361d76f0a74add960c89f945d13bdb872b11
SeEun Park <pse6704@sookmyung.ac.kr>
2025-08-23 17:01:43 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 56 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,12 +1,13 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
+
 N, K = map(int, input().split())
 q = deque(range(1, N+1))
-result = []
-while q:
-    q.rotate(-(K-1))
-    result.append(q.popleft())
 
-print("<"+', '.join(map(str,result))+">")
+result = []
+while q: 
+    q.rotate(-K)
+    result.append(q.pop())
+
+print("<"+ ', '.join(map(str,result))+ ">")
